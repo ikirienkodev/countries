043f290923151ff3350a5771be613e708443e4a9
ikirienkodev <ikirienkodev@gmail.com>
2022-11-10 22:27:49 +0300

    increase timeout

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 export const authApiInstance = axios.create({
   baseURL: 'https://reqres.in/api',
-  timeout: 2000,
+  timeout: 5000,
 });
 
 export const countriesApiInstance = axios.create({
   baseURL: 'https://restcountries.com/v3.1',
-  timeout: 2000,
+  timeout: 5000,
 });
